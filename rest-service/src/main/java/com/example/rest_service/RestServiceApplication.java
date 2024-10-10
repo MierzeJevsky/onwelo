13254e201546a59dc25d1ecb4115b2a5bca287b1
MierzeJevsky <mikolaj.mierzej@gmail.com>
2024-10-10 22:24:58 +0200

    Cleaning

--- rest-service/src/main/java/com/example/rest_service/RestServiceApplication.java
+++ rest-service/src/main/java/com/example/rest_service/RestServiceApplication.java
@@ -1,16 +1,10 @@
 package com.example.rest_service;
 
-import org.apache.catalina.filters.CorsFilter;
-import org.hibernate.engine.internal.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.expression.Arrays;
 
 @SpringBootApplication
 public class RestServiceApplication {
