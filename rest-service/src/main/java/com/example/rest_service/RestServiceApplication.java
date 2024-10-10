d4088ed3cba2fbc192e20ea7a0a16804af974d6d
MierzeJevsky <mikolaj.mierzej@gmail.com>
2024-10-10 22:23:40 +0200

    Configurate CORS

--- rest-service/src/main/java/com/example/rest_service/RestServiceApplication.java
+++ rest-service/src/main/java/com/example/rest_service/RestServiceApplication.java
@@ -1,7 +1,16 @@
 package com.example.rest_service;
 
+import org.apache.catalina.filters.CorsFilter;
+import org.hibernate.engine.internal.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.expression.Arrays;
 
 @SpringBootApplication
 public class RestServiceApplication {
@@ -9,5 +18,16 @@ public class RestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
+	
+	@Component
+    public class WebConfig implements WebMvcConfigurer {
+    @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS", "HEAD");
+        }
+    
+    }
 
 }
